29d9a43f3d36d84bf7b6651e4553a30718a12ab2
Piotr Tomaszewski <46851343+DANIEC7909@users.noreply.github.com>
2024-01-04 22:30:45 +0100

    Filling node script

--- Assets/Scripts/AStar/Node.cs
+++ Assets/Scripts/AStar/Node.cs
@@ -2,17 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : MonoBehaviour
+public class Node 
 {
-    // Start is called before the first frame update
-    void Start()
+    public int GCost, HCost;
+    public bool IsWalkable;
+    public Vector3 WorldPosition;
+
+    public Vector2 GridCordinates;
+
+    public Node Parent;
+
+    public Node(bool _isWalkable, Vector3 _worldPos, Vector2 _GridCordinates)
     {
-        
+        IsWalkable = _isWalkable;
+        WorldPosition = _worldPos;
+        GridCordinates = _GridCordinates;
     }
 
-    // Update is called once per frame
-    void Update()
+    public int FCost
     {
-        
+        get
+        {
+            return GCost + HCost;
+        }
+
     }
 }
