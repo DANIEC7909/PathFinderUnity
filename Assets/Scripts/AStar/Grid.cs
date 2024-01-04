8a42d970f4c69005b8de0a6d6ca2c918e957751f
Piotr Tomaszewski <46851343+DANIEC7909@users.noreply.github.com>
2024-01-04 23:42:06 +0100

    Grid and Node
    Filling up Grid script and Node script

--- Assets/Scripts/AStar/Grid.cs
+++ Assets/Scripts/AStar/Grid.cs
@@ -1,18 +1,87 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Tooltip("We just using X and Z")]
+    public Vector3 GridWorldSize;
+    public float NodeRadius;
+    public Node[,] NodeGrid;
+    public Vector2Int GridSize;
+    public LayerMask ObstacleMask;
+    Vector3 worldBottomLeft;
+   [SerializeField] float nodeDiameter;
+
+    private void Awake()
     {
+        CalculateGridSize();
+        CreateGrid();
+    }
+   
+   [MethodImpl(MethodImplOptions.AggressiveInlining)]
+   public void CalculateGridSize()
+   {
+        nodeDiameter = Mathf.Pow(NodeRadius, 2);
+
+        GridSize = new Vector2Int(Mathf.RoundToInt(GridWorldSize.x/nodeDiameter), Mathf.RoundToInt(GridWorldSize.z / nodeDiameter));
+   }
+
+   public void CreateGrid()
+   {
+        NodeGrid = new Node[GridSize.x, GridSize.y];
         
+        worldBottomLeft = transform.position - ((Vector3.right * GridWorldSize.x / 2) + (Vector3.forward * GridWorldSize.z / 2));
+   
+        for(int x = 0;x<GridSize.x;x++)
+        {
+            for(int y = 0; y < GridSize.y;y++)
+            {
+                Vector3 calculatedWorldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + NodeRadius) + Vector3.forward * (y * nodeDiameter + NodeRadius);
+               // Vector3 worldPoint = new Vector3(calculatedWorldPoint.x,0, calculatedWorldPoint.y);
+
+                bool isWalkable = Physics.OverlapSphere(calculatedWorldPoint, NodeRadius, ObstacleMask).Length > 0 ? false : true;
+              
+                NodeGrid[x, y] = new Node(isWalkable, calculatedWorldPoint, new Vector2Int(x, y));
+            }
+        }
+    
     }
 
-    // Update is called once per frame
-    void Update()
+   public Node GetNodeFromWorldPosition(Vector3 worldPosition)
     {
-        
+        return null;
+    }
+   public List<Node> GetNeighbors(Node node)
+    {
+        return null;
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, .92f, .016f, .3f);
+
+        Gizmos.DrawCube(transform.position + Vector3.up*.05f, GridWorldSize);
+
+        Gizmos.color = Color.red;
+        if (NodeGrid!=null && NodeGrid.Length > 0)
+        {
+            for (int x = 0; x < GridSize.x; x++)
+            {
+                for (int y = 0; y < GridSize.y; y++)
+                {
+                    if (NodeGrid[x, y].IsWalkable)
+                    {
+                        Gizmos.color = Color.red;
+                        Gizmos.DrawWireCube(NodeGrid[x, y].WorldPosition, Vector3.one * (NodeRadius - .2f));
+                    }
+                    else
+                    {
+                        Gizmos.color = Color.black;
+                        Gizmos.DrawCube(NodeGrid[x, y].WorldPosition, Vector3.one * (NodeRadius - .2f));
+                    }
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/AStar/Node.cs
+++ Assets/Scripts/AStar/Node.cs
@@ -1,18 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
+[System.Serializable]
 public class Node 
 {
     public int GCost, HCost;
     public bool IsWalkable;
     public Vector3 WorldPosition;
 
-    public Vector2 GridCordinates;
+    public Vector2Int GridCordinates;
 
     public Node Parent;
 
-    public Node(bool _isWalkable, Vector3 _worldPos, Vector2 _GridCordinates)
+    public Node(bool _isWalkable, Vector3 _worldPos, Vector2Int _GridCordinates)
     {
         IsWalkable = _isWalkable;
         WorldPosition = _worldPos;
