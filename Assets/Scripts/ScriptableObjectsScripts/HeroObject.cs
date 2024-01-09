31d7ecae362bb7937d06247e02bc309bd97602cb
Piotr Tomaszewski <46851343+DANIEC7909@users.noreply.github.com>
2024-01-09 17:51:50 +0100

    added skin to the  HeroObject script

--- Assets/Scripts/ScriptableObjectsScripts/HeroObject.cs
+++ Assets/Scripts/ScriptableObjectsScripts/HeroObject.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "HeroData", menuName = "ScriptableObjects/Hero", order = 1)]
 public class HeroObject : ScriptableObject
 {
     public int HeroID;
     public Sprite HeroImage;
     public string HeroName;
     public HeroStatistics HeroMaxStatistics;
+    public Material HeroSkin;
     public string PrefabKey;
+
 }
