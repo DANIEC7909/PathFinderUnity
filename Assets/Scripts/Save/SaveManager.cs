6c9c68dd58e55a7475ae8377bc26c8e8048057b4
Piotr Tomaszewski <46851343+DANIEC7909@users.noreply.github.com>
2024-01-12 00:18:41 +0100

    Update SaveManager.cs
    Changed how stats are loaded from save

--- Assets/Scripts/Save/SaveManager.cs
+++ Assets/Scripts/Save/SaveManager.cs
@@ -1,45 +1,37 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
 public class SaveManager : Singleton<SaveManager>
 {
     public ISaveLayer SaveLayer;
-    
+
     public void Awake()
     {
         Init(this);
         SaveLayer = new JSON_SaveLayer();
     }
-  
+
     public void Save()
     {
         SaveData saveData = new SaveData();
-        saveData.MapSeed = 123123123;
+        saveData.MapSeed = GameController.Instance.MapSeed;
         saveData.HeroStatistics = new HeroStatisticsSave[GameController.Instance.AllSpawnedHeros.Count];
-        for(int i =0; i<GameController.Instance.AllSpawnedHeros.Count;i++)
+        for (int i = 0; i < GameController.Instance.AllSpawnedHeros.Count; i++)
         {
             saveData.HeroStatistics[i].HeroID = GameController.Instance.AllSpawnedHeros[i].SO.HeroID;
             saveData.HeroStatistics[i].HeroStatistics.Health = GameController.Instance.AllSpawnedHeros[i].CurrentHeroStatistics.Health;
             saveData.HeroStatistics[i].HeroStatistics.Speed = GameController.Instance.AllSpawnedHeros[i].CurrentHeroStatistics.Speed;
             saveData.HeroStatistics[i].HeroStatistics.Strength = GameController.Instance.AllSpawnedHeros[i].CurrentHeroStatistics.Strength;
         }
-     SaveLayer.PraseData(saveData);
+        SaveLayer.PraseData(saveData);
     }
-    public void  Load()
+    public void Load()
     {
-      SaveData saveData= SaveLayer.ReadData();
-        foreach(Hero h in GameController.Instance.AllSpawnedHeros)
+        SaveData saveData = SaveLayer.ReadData();
+        GameController.Instance.MapSeed = saveData.MapSeed;
+
+        for (int i = 0; i < GameController.Instance.AllSpawnedHeros.Count; i++)
         {
-            foreach(HeroStatisticsSave hss in saveData.HeroStatistics)
-            {
-                if(h.SO.HeroID== hss.HeroID)
-                {
-                    h.CurrentHeroStatistics.Health = hss.HeroStatistics.Health;
-                    h.CurrentHeroStatistics.Strength = hss.HeroStatistics.Strength;
-                    h.CurrentHeroStatistics.Speed = hss.HeroStatistics.Speed;
-                }
-            }
+            GameController.Instance.AllSpawnedHeros[i].CurrentHeroStatistics.Health = saveData.HeroStatistics[i].HeroStatistics.Health;
+            GameController.Instance.AllSpawnedHeros[i].CurrentHeroStatistics.Speed = saveData.HeroStatistics[i].HeroStatistics.Speed;
+            GameController.Instance.AllSpawnedHeros[i].CurrentHeroStatistics.Strength = saveData.HeroStatistics[i].HeroStatistics.Strength;
         }
     }
 }
