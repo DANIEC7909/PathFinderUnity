fffba1263b299c2528441a37794fd45639aed233
Piotr Tomaszewski <46851343+DANIEC7909@users.noreply.github.com>
2024-01-12 00:46:20 +0100

    Added Binary SaveLLayer

---
+++ Assets/Scripts/Save/Binary_SaveLayer.cs
@@ -0,0 +1,45 @@
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class Binary_SaveLayer : ISaveLayer
+{
+    string path = Application.persistentDataPath;
+    public async void PraseData(SaveData saveData)
+    {
+        await Task.Run(() =>
+        {
+            Parallel.Invoke(() =>
+            {
+                System.IO.Stream ms = File.OpenWrite(path + @"/Savebin.pfbin");
+
+                BinaryFormatter formatter = new BinaryFormatter();
+                //It serialize the employee object  
+                formatter.Serialize(ms, saveData);
+                ms.Flush();
+                ms.Close();
+                ms.Dispose();
+                GameEvents.Instance.c_OnGameSaved(saveData);
+            });
+        });
+    }
+
+    public SaveData ReadData()
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+
+        FileStream fs = File.Open(path + @"/Savebin.pfbin", FileMode.Open);
+
+        object obj = formatter.Deserialize(fs);
+        SaveData data = (SaveData)obj;
+        fs.Flush();
+        fs.Close();
+        fs.Dispose();
+        GameEvents.Instance.c_OnSaveLoaded(data);
+        return data;
+    }
+
+
+}
